37fe9e7977e34084e52942ec8f18352517b377d6
fleish <2106430+fleish@users.noreply.github.com>
2025-07-11 12:14:45 -0400

    Update sync.sh
    fix appNewVersion for new XML feed URL and to only pull new versions of the desktop app

--- fragments/labels/sync.sh
+++ fragments/labels/sync.sh
@@ -2,7 +2,7 @@ sync)
     name="Sync"
     type="dmg"
     downloadURL="https://www.sync.com/download/apple/Sync.dmg"
-    appNewVersion="$(curl -fs "https://www.sync.com/blog/category/desktop/feed/" | xpath '(//channel/item/title)[1]' 2>/dev/null | sed -E 's/^.* ([0-9.]*) .*$/\1/g')"
+    appNewVersion="$(curl -fs "https://www.sync.com/blog/tag/desktop/rss.xml" | grep "Desktop App.* Available" | head -1 | awk '{print $2}')"
     expectedTeamID="7QR39CMJ3W"
     blockingProcesses=( "Sync" "sync-worker.exe" )
     ;;
